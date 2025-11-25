0d62f426bc59ee18fc19c80f1c326d3a8343ee7b
Emil <emjo0002@stud.kea.dk>
2025-11-25 11:16:25 +0100

    x

---
+++ dictionary.py
@@ -0,0 +1,188 @@
+allowed_languages = ["english", "danish", "spanish"]
+
+email = {
+    "en" : "Email",
+    "dk" : "Email",
+    "sp" : "Correo electrónico"
+}
+
+username = {
+    "en" : "Username",
+    "dk" : "Brugernavn",
+    "sp" : "Nombre de usuario"
+}
+
+first_name = {
+    "en" : "First name",
+    "dk" : "Fornavn",
+    "sp" : "Nombre"
+}
+
+last_name = {
+    "en" : "Last name",
+    "dk" : "Efternavn",
+    "sp" : "Apellido"
+}
+
+
+password = {
+    "en" : "Password",
+    "dk" : "Pinkode",
+    "sp" : "Clave"
+}
+
+login = {
+    "en" : "Login",
+    "dk" : "Log ind",
+    "sp" : "Entrar"
+}
+
+characters = {
+    "en": "Characters",
+    "dk": "Tegn",
+    "sp": "Caracteres"
+}
+
+to = {
+    "en": "to",
+    "dk": "til",
+    "sp": "a"
+}
+
+dont_have_an_account = {
+    "en" : "Don't have an account",
+    "dk" : "Har du ikke en konto",
+    "sp" : "No tienes cuenta"
+}
+
+update_profile = {
+    "en": "Update profile",
+    "dk": "Opdater profil",
+    "sp": "Actualizar perfil"
+}
+
+updating_profile = {
+    "en": "Updating profile ...",
+    "dk": "Opdatere profil ...",
+    "sp": "Actualizar perfil ..."
+}
+
+profile_updated_successfully = {
+    "en": "Profile updated successfully",
+    "dk": "Din profil er opdateret",
+    "sp": "Actualizar perfil"
+}
+
+email_already_registered = {
+    "en": "Email already registered",
+    "dk": "Email er allerede registreret",
+    "sp": "El correo electrónico ya está registrado"
+}
+
+username_already_registered = {
+    "en": "Username already registered",
+    "dk": "Brugernavn er allerede registreret",
+    "sp": "El nombre de usuario ya está registrado"
+}
+
+system_under_maintenance = {
+    "en": "System under maintenance",
+    "dk": "Systemet er under vedligeholdelse",
+    "sp": "El sistema está en mantenimiento"
+}
+
+#Home messages
+
+home = {
+    "en": "Home",
+    "dk": "Hjem",
+    "sp": "Casa"
+}
+
+explore = {
+    "en": "Explore",
+    "dk": "Udforsk",
+    "sp": "Explorar"
+}
+
+notifications = {
+    "en": "Notifications",
+    "dk": "Notifikationer",
+    "sp": "Notificaciones"
+}
+
+message = {
+    "en": "Messages",
+    "dk": "Beskeder",
+    "sp": "Mensajes"
+}
+
+bookmarks = {
+    "en": "Bookmarks",
+    "dk": "Bogmærker",
+    "sp": "Marcadores"
+}
+
+jobs = {
+    "en": "Jobs",
+    "dk": "Job",
+    "sp": "Empleos"
+}
+
+communities = {
+    "en": "Communities",
+    "dk": "Fællesskaber",
+    "sp": "Comunidades"
+}
+
+profile = {
+    "en": "Profile",
+    "dk": "Profil",
+    "sp": "Perfil"
+}
+
+more = {
+    "en": "More",
+    "dk": "Mere",
+    "sp": "Más"
+}
+
+logout = {
+    "en": "Logout",
+    "dk": "Log ud",
+    "sp": "Cerrar sesión"
+}
+
+# error messages
+
+invalid_email = {
+    "en" : "Invalid email",
+    "dk" : "Ugyldig e-mail",
+    "sp" : "Correo electrónico inválido"
+}
+
+invalid_password = {
+    "en" : "Invalid password",
+    "dk" : "Ugyldig pinkode",
+    "sp" : "Clave inválida"
+}
+
+invalid_credentials = {
+    "en" : "Invalid credentials",
+    "dk" : "Ugyldige loginoplysninger",
+    "sp" : "Información incorrecta"    
+}
+
+user_not_verified = {
+    "en" : "User not verified. Please check your email",
+    "dk" : "Bruger ikke verificeret. Tjek venligst din e-mail",
+    "sp" : "Usuario no verificado. Por favor, chequéa tu email"    
+}
+
+user_not_found = {
+    "en" : "User not found",
+    "dk" : "Bruger ikke fundet",
+    "sp" : "Usuario no encontrado"
+}
+
+
